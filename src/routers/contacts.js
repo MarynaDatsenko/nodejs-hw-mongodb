7492bacf8e257c1df5efd01cfc234bd105730c11
marynad <mskameneva@gmail.com>
2025-06-05 23:04:14 +0300

    hw-5

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,14 +12,17 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { createContactSchema } from '../validation/contacts.js';
 import { updateContactSchema } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
-router.get('/contacts', ctrlWrapper(getContactsController));
+router.use(authenticate);
+
+router.get('/', ctrlWrapper(getContactsController));
 
 router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
 
-router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactsController));
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactsController));
 
 router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 
